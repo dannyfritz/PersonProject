c7196f50903525c501c842f7224e37f7de328464
Tim Garcia <tim.m.garcia@gmail.com>
2016-02-25 10:25:33 -0800

    [#113116721] Verify Old password when changing
    The story was restarted to fix a bug.  There is
    now user feedback if the password change failed.

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -480,7 +480,9 @@ app.controller('UserPasswordController', ["$scope", "$state", "UsersService",
     $scope.change = function(password, new_password){
       UsersService.changePassword(password, new_password).then(function(){
         $state.go('user.dashboard');
-      });
+      }).catch(function() {
+        $scope.errorMessage = "Old/New password is invalid";
+      })
     };
 }]);
 
@@ -495,7 +497,9 @@ app.controller('UserProfileController', ["$scope", "$state", "UsersService",
         return UsersService.verifyLogin();
       }).then(function(){
         $state.go('user.dashboard');
-      });
+      }).catch(function() {
+        $scope.errorMessage = "Password is invalid";
+      })
     };
 }]);
 
